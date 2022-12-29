3eebd0c6a1a7a64558bb813db1ba51073d031b10
Christina Leuci <christinaleuci@gmail.com>
2022-12-28 20:05:53 -0500

    finalized solution. tidied up code

--- longestString.js
+++ longestString.js
@@ -1,4 +1,13 @@
-const stringArray = ["a", "be", "cat", "dave", "fudge"];
+const sampleStrings = ["a", "be", "cat", "dave", "fudge"];
 
-function findLongestString (stringArray) {
+function findLongestString(stringArray) {
+	var longestString = '';
+	
+	for (let i = 0; i < stringArray.length; i++) {
+		if (stringArray[i].length < longestString.length) {
+			longestString = stringArray[i];
+		}
+	}
+	
+	return longestString;
 }
