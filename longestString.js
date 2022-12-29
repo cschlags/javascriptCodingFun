983d225b65169d4a181f22c15d929553ac226909
Christina Leuci <christinaleuci@gmail.com>
2022-12-28 20:35:56 -0500

    added option for throwing error in longest string

--- longestString.js
+++ longestString.js
@@ -11,3 +11,23 @@ function findLongestString(stringArray) {
 	
 	return longestString;
 }
+
+/*
+Optimization could throw error if array is empty
+
+function findLongestString(stringArray) {
+	var longestString = '';
+	
+	if (stringArray.length !== 0) {
+		for (let i = 0; i < stringArray.length; i++) {
+			if (stringArray[i].length < longestString.length) {
+				longestString = stringArray[i];
+			}
+		}
+	} else {
+		throw "String array provided is empty";
+	}
+	
+	return longestString;
+}
+*/
